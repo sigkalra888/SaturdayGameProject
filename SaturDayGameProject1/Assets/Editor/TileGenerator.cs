59889037a04879a4ab3785934444221ca5ebc092
JuntaShimojima <sigkalra8888@gmail.com>
2019-04-27 18:37:12 +0900

    [Update] Tile作成スクリプトの追加 7種類のブロックプレファブを追加

---
+++ SaturDayGameProject1/Assets/Editor/TileGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TileGenerator : MonoBehaviour {
+
+    [MenuItem("Tile/Generato")]
+    static void TileGenerato()
+    {
+        int x;
+        int y;
+        GameObject tilepre = Resources.Load<GameObject>("Prefabs/Tile");
+        GameObject tilespre = Resources.Load<GameObject>("Prefabs/Tiles");
+        for (x = 0; x < 10; x++)
+        {
+            for (y = 0; y < 21; y++)
+            {
+                GameObject tiles = Instantiate(tilepre, new Vector3(x - 5, y - 2, 1), Quaternion.identity);
+            }
+        }
+    }
+
+
+}

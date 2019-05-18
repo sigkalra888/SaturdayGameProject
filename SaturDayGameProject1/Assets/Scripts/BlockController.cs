90ca3869fd61c1ad2db114ff3bf8d3d4d9f7134b
shimojima <sigkalra8888@gmail.com>
2019-05-18 18:01:59 +0900

    [New] BlockManagerとBlockControllerの作成開始

---
+++ SaturDayGameProject1/Assets/Scripts/BlockController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    private float time;
+    private float tmpTime = 0.7f;
+    private float intarval = 0.7f;
+    [SerializeField]
+    private GameObject[] blocks;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //移動処理
+    private void Fall()
+    {
+        time += Time.deltaTime;
+        if (time >= tmpTime)
+        {
+            tmpTime = time + intarval;
+            this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x,
+                                                                  this.gameObject.transform.localPosition.y - 1,
+                                                                  this.gameObject.transform.localPosition.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x - 1,
+                                                                  this.gameObject.transform.localPosition.y,
+                                                                  this.gameObject.transform.localPosition.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x + 1,
+                                                                  this.gameObject.transform.localPosition.y,
+                                                                  this.gameObject.transform.localPosition.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            this.gameObject.transform.localEulerAngles = new Vector3(0, 0, this.gameObject.transform.localEulerAngles.z - 90);
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            this.gameObject.transform.localEulerAngles = new Vector3(0, 0, this.gameObject.transform.localEulerAngles.z + 90);
+        }
+    }
+}
---
+++ SaturDayGameProject1/Assets/Scripts/BlockManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] tetoBlock;
+    [SerializeField]
+    private List<GameObject> stockBlocks;
+    [SerializeField]
+    private GameObject block;
+
+    private float time;
+
+    //ストックブロック生成の為の変数
+    private int y = 17;
+    private int count = 0;
+
+    private bool blockfalling = true;
+
+    void Start()
+    {
+        Invoke("FirstBlockGenerator", 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        StockBlockGenerator();
+    }
+
+    private void FirstBlockGenerator()
+    {
+        int tetoNum = Random.Range(0, tetoBlock.Length);
+        GameObject blockPre = Instantiate(tetoBlock[tetoNum], new Vector3(-1, 16, 0.45f), Quaternion.identity);
+        block = blockPre;
+        blockfalling = false;
+       
+    }
+
+    private void StockBlockGenerator()
+    {
+        
+        if (time >= 2 && stockBlocks.Count != 5)
+        {
+            int tetoNum = Random.Range(0, tetoBlock.Length);
+            GameObject blockPre = Instantiate(tetoBlock[tetoNum], new Vector3(7, y - count * 2, 0.45f), Quaternion.identity);
+            blockPre.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            stockBlocks.Add(blockPre);
+            count++;
+        }
+    }
+}

ed064f5dc0958926ff716d63ab4b363b055fee4a
shimojima <sigkalra8888@gmail.com>
2019-05-25 18:50:40 +0900

    [Update] スクリプトの調整

--- SaturDayGameProject1/Assets/Scripts/BlockController.cs
+++ SaturDayGameProject1/Assets/Scripts/BlockController.cs
@@ -4,54 +4,158 @@ using UnityEngine;
 
 public class BlockController : MonoBehaviour
 {
+    public BlockManager blockManager;
     private float time;
     private float tmpTime = 0.7f;
     private float intarval = 0.7f;
     [SerializeField]
-    private GameObject[] blocks;
+    public GameObject[] blocks;
+    public bool fallstate = false;
+    public int myIndex;
+    private int myState = 0;
+    public int[,] blockPosNow;
+    public int[,] blockPosDef;
+    public int[,] blockPosR;
+    public int[,] blockPosL;
+    public int[,] blockPosU;
 
     void Start()
     {
-        
+        blockPosNow = new int[4, 4];
+        blockPosNow = blockPosDef;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Fall();
     }
 
     //移動処理
     private void Fall()
     {
-        time += Time.deltaTime;
-        if (time >= tmpTime)
+        if (fallstate)
         {
-            tmpTime = time + intarval;
-            this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x,
-                                                                  this.gameObject.transform.localPosition.y - 1,
-                                                                  this.gameObject.transform.localPosition.z);
-        }
+            time += Time.deltaTime;
+            if (time >= tmpTime)
+            {
+                tmpTime = time + intarval;
+                blockManager.PosUpdate();
+                this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x,
+                                                                      this.gameObject.transform.localPosition.y - 1,
+                                                                      this.gameObject.transform.localPosition.z);
+            }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x - 1,
-                                                                  this.gameObject.transform.localPosition.y,
-                                                                  this.gameObject.transform.localPosition.z);
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x - 1,
+                                                                      this.gameObject.transform.localPosition.y,
+                                                                      this.gameObject.transform.localPosition.z);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x + 1,
+                                                                      this.gameObject.transform.localPosition.y,
+                                                                      this.gameObject.transform.localPosition.z);
+            }
+            
+            switch (myIndex)
+            {
+                case 1:
+                    BlockRotation1(myState);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+    }
+
+    private void BlockRotation1(int x)
+    {
+        switch (x)
         {
-            this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x + 1,
-                                                                  this.gameObject.transform.localPosition.y,
-                                                                  this.gameObject.transform.localPosition.z);
-        }
-        else if (Input.GetKeyDown(KeyCode.Z))
-        {
-            this.gameObject.transform.localEulerAngles = new Vector3(0, 0, this.gameObject.transform.localEulerAngles.z - 90);
-        }
-        else if (Input.GetKeyDown(KeyCode.X))
-        {
-            this.gameObject.transform.localEulerAngles = new Vector3(0, 0, this.gameObject.transform.localEulerAngles.z + 90);
+            case 0:
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    blockPosNow = blockPosL;
+                    myState = 3;
+                    blocks[0].transform.localPosition = new Vector3(0, -1, 0);
+                    blocks[2].transform.localPosition = new Vector3(0, 1, 0);
+                    blocks[3].transform.localPosition = new Vector3(-1, 0, 0);
+                }
+                else if (Input.GetKeyDown(KeyCode.X))
+                {
+                    blockPosNow = blockPosR;
+                    myState = 1;
+                    blocks[0].transform.localPosition = new Vector3(0, 1, 0);
+                    blocks[2].transform.localPosition = new Vector3(0, -1, 0);
+                    blocks[3].transform.localPosition = new Vector3(1, 0, 0);
+                }
+                break;
+            case 1:
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    blockPosNow = blockPosDef;
+                    myState = 0;
+                    blocks[0].transform.localPosition = new Vector3(-1, 0, 0);
+                    blocks[2].transform.localPosition = new Vector3(1, 0, 0);
+                    blocks[3].transform.localPosition = new Vector3(0, 1, 0);
+                }
+                else if (Input.GetKeyDown(KeyCode.X))
+                {
+                    blockPosNow = blockPosU;
+                    myState = 2;
+                    blocks[0].transform.localPosition = new Vector3(1, 0, 0);
+                    blocks[2].transform.localPosition = new Vector3(-1, 0, 0);
+                    blocks[3].transform.localPosition = new Vector3(0, -1, 0);
+                }
+                break;
+            case 2:
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    blockPosNow = blockPosR;
+                    myState = 1;
+                    blocks[0].transform.localPosition = new Vector3(0, 1, 0);
+                    blocks[2].transform.localPosition = new Vector3(0, -1, 0);
+                    blocks[3].transform.localPosition = new Vector3(1, 0, 0);
+                }
+                else if (Input.GetKeyDown(KeyCode.X))
+                {
+                    blockPosNow = blockPosL;
+                    myState = 3;
+                    blocks[0].transform.localPosition = new Vector3(0, -1, 0);
+                    blocks[2].transform.localPosition = new Vector3(0, 1, 0);
+                    blocks[3].transform.localPosition = new Vector3(-1, 0, 0);
+                }
+                break;
+            case 3:
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    blockPosNow = blockPosU;
+                    myState = 2;
+                    blocks[0].transform.localPosition = new Vector3(1, 0, 0);
+                    blocks[2].transform.localPosition = new Vector3(-1, 0, 0);
+                    blocks[3].transform.localPosition = new Vector3(0, -1, 0);
+                }
+                else if (Input.GetKeyDown(KeyCode.X))
+                {
+                    blockPosNow = blockPosDef;
+                    myState = 0;
+                    blocks[0].transform.localPosition = new Vector3(-1, 0, 0);
+                    blocks[2].transform.localPosition = new Vector3(1, 0, 0);
+                    blocks[3].transform.localPosition = new Vector3(0, 1, 0);
+                }
+                break;
         }
     }
 }
--- SaturDayGameProject1/Assets/Scripts/BlockManager.cs
+++ SaturDayGameProject1/Assets/Scripts/BlockManager.cs
@@ -11,20 +11,42 @@ public class BlockManager : MonoBehaviour
     [SerializeField]
     private GameObject block;
 
+    private int[,] stage = new int[21, 10]
+    {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
     private float time;
 
     //ストックブロック生成の為の変数
     private int y = 17;
     private int count = 0;
 
-    private bool blockfalling = true;
-
     void Start()
     {
         Invoke("FirstBlockGenerator", 2f);
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         time += Time.deltaTime;
@@ -33,10 +55,12 @@ public class BlockManager : MonoBehaviour
 
     private void FirstBlockGenerator()
     {
-        int tetoNum = Random.Range(0, tetoBlock.Length);
+        int tetoNum = 0;//Random.Range(0, tetoBlock.Length);
         GameObject blockPre = Instantiate(tetoBlock[tetoNum], new Vector3(-1, 16, 0.45f), Quaternion.identity);
+        BlockPosSet(blockPre, tetoNum);
+        blockPre.GetComponent<BlockController>().blockManager = this.gameObject.GetComponent<BlockManager>();
+        blockPre.GetComponent<BlockController>().fallstate = true;
         block = blockPre;
-        blockfalling = false;
        
     }
 
@@ -45,11 +69,76 @@ public class BlockManager : MonoBehaviour
         
         if (time >= 2 && stockBlocks.Count != 5)
         {
-            int tetoNum = Random.Range(0, tetoBlock.Length);
+            int tetoNum = 0;//Random.Range(0, tetoBlock.Length);
             GameObject blockPre = Instantiate(tetoBlock[tetoNum], new Vector3(7, y - count * 2, 0.45f), Quaternion.identity);
+            BlockPosSet(blockPre, tetoNum);
             blockPre.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
             stockBlocks.Add(blockPre);
             count++;
+        }
+    }
+
+    private void BlockPosSet(GameObject pre, int x)
+    {
+        switch (x + 1)
+        {
+            case 1:
+                pre.GetComponent<BlockController>().blockPosDef = new int[4, 4]
+                {
+                    {0, 1, 0, 0},
+                    {1, 1, 1, 0},
+                    {0, 0, 0, 0},
+                    {0, 0, 0, 0}
+                };
+                pre.GetComponent<BlockController>().blockPosR = new int[4, 4]
+                {
+                    {0, 1, 0, 0},
+                    {0, 1, 1, 0},
+                    {0, 1, 0, 0},
+                    {0, 0, 0, 0}
+                };
+                pre.GetComponent<BlockController>().blockPosL = new int[4, 4]
+                {
+                    {0, 1, 0, 0},
+                    {1, 1, 0, 0},
+                    {0, 1, 0, 0},
+                    {0, 0, 0, 0}
+                };
+                pre.GetComponent<BlockController>().blockPosU = new int[4, 4]
+                {
+                    {0, 0, 0, 0},
+                    {1, 1, 1, 0},
+                    {0, 1, 0, 0},
+                    {0, 0, 0, 0}
+                };
+                pre.GetComponent<BlockController>().myIndex = 1;
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+        }
+    }
+
+    public void PosUpdate()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if ((int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.y + (int)block.transform.localPosition.y - 1 < 0 || (int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.x + (int)block.transform.localPosition.x + 1 > 10 || (int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.x + (int)block.transform.localPosition.x - 1< 0)
+            {
+                block.GetComponent<BlockController>().fallstate = false;
+                break;
+            }
+            stage[(int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.y + (int)block.transform.localPosition.y,
+                  (int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.x + (int)block.transform.localPosition.x] = block.GetComponent<BlockController>().myIndex;
         }
     }
 }

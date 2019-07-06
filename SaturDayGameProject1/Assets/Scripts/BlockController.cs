c47c7f40fb37349e9d68412c4742eb368145bf37
shimojima <sigkalra8888@gmail.com>
2019-07-06 18:44:01 +0900

    [Update] 頭痛い

--- SaturDayGameProject1/Assets/Scripts/BlockController.cs
+++ SaturDayGameProject1/Assets/Scripts/BlockController.cs
@@ -9,27 +9,47 @@ public class BlockController : MonoBehaviour
     [SerializeField]
     public GameObject[] blocks;
     public bool fallState = false;
-    public bool sideMoveState = false;
-    public bool rotatinState = false;
     public bool kidou = false;
     public int myIndex;
     private int myState = 0;
+    [HideInInspector]
+    public bool firstTime = false;
     public int[,] blockPosNow;
     public int[,] blockPosDef;
     public int[,] blockPosR;
     public int[,] blockPosL;
     public int[,] blockPosU;
 
-    void Start()
+    private void Start()
     {
-        blockPosNow = new int[4, 4];
-        blockPosNow = blockPosDef;
+        for (int i = 0; i < 4; i++)
+        {
+            blockManager.posManager[blockManager.count].tetoBlockBasePos[(int)blocks[i].transform.localPosition.y, 
+                                                                         (int)blocks[i].transform.localPosition.x] = 1;
+        }
+        blockManager.count++;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!kidou) { return; }
+        if (!firstTime)
+        {
+            blockManager.x = (int)gameObject.transform.localPosition.x;
+            blockManager.y = (int)gameObject.transform.localPosition.y;
+            for (int i = 0; i < 4; i++)
+            {
+                blockManager.tPos[i].x = blockManager.x + (int)blocks[i].transform.localPosition.x;
+                blockManager.tPos[i].y = ((blockManager.y + (int)blocks[i].transform.localPosition.y) - 20) * -1;
+                blockManager.BlockSetForStage(i);
+            }
+            blockPosNow = new int[4, 4];
+            blockPosNow = blockPosDef;
+            blockManager.TextPrint();
+            CanFall();
+            firstTime = true;
+        }
         Fall();
     }
 
@@ -41,73 +61,101 @@ public class BlockController : MonoBehaviour
             time += Time.deltaTime;
             if (time >= blockManager.tmpTime)
             {
-                if (blockManager.PosUpdateF())
+                if (CanFall() == false)
+                {
+                    blockManager.PosUpdateF();
+                }
+                else
                 {
                     blockManager.tmpTime = time + blockManager.intarval;
-                    
+
                     this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x,
-                                                                          this.gameObject.transform.localPosition.y - 1,
+                                                                          this.gameObject.transform.localPosition.y + 1,
                                                                           this.gameObject.transform.localPosition.z);
-                    for (int i = 0; i < 4; ++i)
-                    {
-                        blockManager.BlockSetForStage(i);
-                    }
                 }
+
+                for (int i = 0; i < 4; ++i)
+                {
+                    blockManager.BlockSetForStage(i);
+                }
+
+                blockManager.TextPrint();
             }    
         }
-
-        if (sideMoveState)
+        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if (blockManager.PosUpdateS() == (int)BlockManager.SideMoveStop.Move || blockManager.PosUpdateS() == (int)BlockManager.SideMoveStop.Rstop)
             {
-                if (blockManager.PosUpdateS() == (int)BlockManager.SideMoveStop.Move || blockManager.PosUpdateS() == (int)BlockManager.SideMoveStop.Rstop)
+                this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x - 1,
+                                                                  this.gameObject.transform.localPosition.y,
+                                                                  this.gameObject.transform.localPosition.z);
+                for (int i = 0; i < 4; ++i)
                 {
-                    this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x - 1,
-                                                                      this.gameObject.transform.localPosition.y,
-                                                                      this.gameObject.transform.localPosition.z);
-                    for (int i = 0; i < 4; ++i)
-                    {
-                        blockManager.BlockSetForStage(i);
-                    }
+                    blockManager.BlockSetForStage(i);
                 }
-                
             }
-            else if (Input.GetKeyDown(KeyCode.RightArrow))
+
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (blockManager.PosUpdateS() == (int)BlockManager.SideMoveStop.Move || blockManager.PosUpdateS() == (int)BlockManager.SideMoveStop.Lstop)
             {
-                if (blockManager.PosUpdateS() == (int)BlockManager.SideMoveStop.Move || blockManager.PosUpdateS() == (int)BlockManager.SideMoveStop.Lstop)
+                this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x + 1,
+                                                                  this.gameObject.transform.localPosition.y,
+                                                                  this.gameObject.transform.localPosition.z);
+                for (int i = 0; i < 4; ++i)
                 {
-                    this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x + 1,
-                                                                      this.gameObject.transform.localPosition.y,
-                                                                      this.gameObject.transform.localPosition.z);
-                    for (int i = 0; i < 4; ++i)
-                    {
-                        blockManager.BlockSetForStage(i);
-                    }
+                    blockManager.BlockSetForStage(i);
                 }
-                
             }
+
         }
 
-        if (rotatinState)
+        RotatedPosSet();
+        
+    }
+
+    private bool CanFall()
+    {
+        for (int i = 0; i < 4; i++)
         {
-            switch (myIndex)
+            int y =  (int)blocks[i].transform.localPosition.y;
+            int x = (int)blocks[i].transform.localPosition.x;
+            if (blockManager.y + y == 0)
             {
-                case 1:
-                    BlockRotation1(myState);
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
+                return false;
             }
+            else if (blockManager.stage[((y + blockManager.y + 1) - 20) * -1, x + blockManager.x] == 9)
+            {
+                return false;
+            }
+            Debug.Log(((y + blockManager.y + 1) - 20) * -1 + ":" + (x + blockManager.x));
+            Debug.Log(blockManager.stage[((y + blockManager.y + 1) - 20) * -1, x + blockManager.x]);
+        }
+
+        return true;
+    }
+
+    private void RotatedPosSet()
+    {
+        switch (myIndex)
+        {
+            case 1:
+                BlockRotation1(myState);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
         }
     }
 
--- SaturDayGameProject1/Assets/Scripts/BlockManager.cs
+++ SaturDayGameProject1/Assets/Scripts/BlockManager.cs
@@ -9,17 +9,14 @@ public class BlockManager : MonoBehaviour
     private GameObject[] tetoBlock;
     [SerializeField]
     private List<GameObject> stockBlocks;
-    [SerializeField]
-    private GameObject block;
+
+    public GameObject block;
+    public int count = 0;
 
     public float tmpTime;
-    private float tmpTime2;
     public float intarval;
-    private float intarval2 = 0.1f;
 
-    private int x, y;
-    private int adjustX = 5;
-    private int adjustY = 2;
+    public int x, y;
 
     public struct TmpPos
     {
@@ -27,7 +24,7 @@ public class BlockManager : MonoBehaviour
         public int y;
     }
 
-    TmpPos[] tPos = new TmpPos[4];
+    public TmpPos[] tPos = new TmpPos[4];
 
     public enum State
     {
@@ -46,7 +43,7 @@ public class BlockManager : MonoBehaviour
     
     public Text[] text;
 
-    private int[,] stage = new int[21, 10]
+    public int[,] stage = new int[21, 10]
     {
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -70,12 +67,18 @@ public class BlockManager : MonoBehaviour
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
     };
+    public struct PosManager
+    {
+        public int[,] tetoBlockBasePos;
+    }
 
-    private float time;
+    public PosManager[] posManager = new PosManager[6];
+
+    public float time;
 
     //ストックブロック生成の為の変数
-    private int high = 17;
-    private int count = 0;
+    private int high = 19;
+    private int count2 = 0;
 
     void Start()
     {
@@ -91,39 +94,26 @@ public class BlockManager : MonoBehaviour
             NextBlock();
         }
 
-        if (time >= tmpTime2)
-        {
-            TextPrint();
-            tmpTime2 = time + intarval2;
-        }
-
         //prefabのX,Y座標
         if(block == null) { return; }
         x = (int)block.transform.localPosition.x;
         y = (int)block.transform.localPosition.y;
-        Debug.Log(BlockHighP((int)block.GetComponent<BlockController>().blocks[3].transform.localPosition.y) + ":" + ((int)block.GetComponent<BlockController>().blocks[3].transform.localPosition.x + x + 5));
+        //Debug.Log(BlockHighP((int)block.GetComponent<BlockController>().blocks[3].transform.localPosition.y) + ":" + ((int)block.GetComponent<BlockController>().blocks[3].transform.localPosition.x + x + 5));
     }
 
     private void FirstBlockGenerator()
     {
         int tetoNum = 0;//Random.Range(0, tetoBlock.Length);
-        GameObject blockPre = Instantiate(tetoBlock[tetoNum], new Vector3(-1, 16, 0.45f), Quaternion.identity);
+        GameObject blockPre = Instantiate(tetoBlock[tetoNum], new Vector3(4, 1, 0.45f), Quaternion.identity);
         BlockPosSet(blockPre, tetoNum);
         blockPre.GetComponent<BlockController>().blockManager = this.gameObject.GetComponent<BlockManager>();
-        blockPre.GetComponent<BlockController>().sideMoveState = true;
         blockPre.GetComponent<BlockController>().fallState = true;
-        blockPre.GetComponent<BlockController>().rotatinState = true;
         blockPre.GetComponent<BlockController>().kidou = true;
         block = blockPre;
         state = State.fall;
         x = (int)block.transform.localPosition.x;
         y = (int)block.transform.localPosition.y;
-        for (int i = 0; i < 4; i++)
-        {
-            tPos[i].x = ((int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.x + x) + adjustX;
-            tPos[i].y = BlockHighP((int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.y);
-            BlockSetForStage(i);
-        }
+        
 
     }
 
@@ -132,11 +122,11 @@ public class BlockManager : MonoBehaviour
         if (time >= 2 && stockBlocks.Count != 5)
         {
             int tetoNum = 0;//Random.Range(0, tetoBlock.Length);
-            GameObject blockPre = Instantiate(tetoBlock[tetoNum], new Vector3(7, high - stockBlocks.Count * 2, 0.45f), Quaternion.identity);
+            GameObject blockPre = Instantiate(tetoBlock[tetoNum], new Vector3(12, high - stockBlocks.Count * 2, 0.45f), Quaternion.identity);
             BlockPosSet(blockPre, tetoNum);
             blockPre.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
             stockBlocks.Add(blockPre);
-            count++;
+            count2++;
         }
     }
 
@@ -212,27 +202,14 @@ public class BlockManager : MonoBehaviour
         return (int)SideMoveStop.Move;
     }
 
-    public bool PosUpdateF()
+    public void PosUpdateF()
     {
-        for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
         {
-            //prefabの中にあるCubeのY座標   以降my
-            int my = (int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.y;
-
-            if (my + y -1 == -3)
-            {
-                block.GetComponent<BlockController>().fallState = false;
-                block.GetComponent<BlockController>().sideMoveState = false;
-                block.GetComponent<BlockController>().rotatinState = false;
-                for (int j = 0; j < 4; j++)
-                {
-                    FinalBlockSetForStage(j);
-                }
-                state = State.next;
-                return false;
-            }
+            FinalBlockSetForStage(j);
         }
-        return true;
+
+        state = State.next;
     }
 
     public void RotationAdjust()
@@ -257,8 +234,8 @@ public class BlockManager : MonoBehaviour
         int mx = (int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.x;
         int my = (int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.y;
 
-        stage[BlockHighP(my) - 1, (mx + x) + adjustX] = 0;
-        stage[BlockHighP(my), (mx + x) + adjustX] = 9;
+        stage[BlockHighP(my) - 1, (mx + x)] = 0;
+        stage[BlockHighP(my), (mx + x)] = 9;
         tmpTime = 0;
     }
 
@@ -267,31 +244,37 @@ public class BlockManager : MonoBehaviour
         int mx = (int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.x;
         int my = (int)block.GetComponent<BlockController>().blocks[i].transform.localPosition.y;
 
-        if (stage[BlockHighP(my), (mx + x) + adjustX] != 9)
+        if (stage[BlockHighP(my), (mx + x)] == 9)
         {
-            stage[tPos[i].y, tPos[i].x] = 0;
-            Debug.Log("stage[" + BlockHighP(my) + "," + ((mx + x) + adjustX) + "]");
-            stage[BlockHighP(my), (mx + x) + adjustX] = block.GetComponent<BlockController>().myIndex;
-            tPos[i].x = (mx + x) + adjustX;
+            block.GetComponent<BlockController>().fallState = false;
+            PosUpdateF();
+        }
+        else
+        {
+            if (block.GetComponent<BlockController>().firstTime)
+            {
+                stage[tPos[i].y, tPos[i].x] = 0;
+            }
+            //Debug.Log("stage[" + BlockHighP(my) + "," + ((mx + x)) + "]");
+            stage[BlockHighP(my), (mx + x)] = block.GetComponent<BlockController>().myIndex;
+            tPos[i].x = (mx + x);
             tPos[i].y = BlockHighP(my);
         }
     }
 
     private int BlockHighP(int my)
     {
-        int _y = (my + y + adjustY) - 20;
+        int _y = (my + y) - 20;
         return _y * -1;
     }
 
     private void NextBlock()
     {
         block = stockBlocks[0];
-        block.transform.localPosition = new Vector3(-1, 16, 0.45f);
+        block.transform.localPosition = new Vector3(3, 18, 0.45f);
         block.transform.localScale = new Vector3(1,1,1);
         block.GetComponent<BlockController>().blockManager = this.gameObject.GetComponent<BlockManager>();
-        block.GetComponent<BlockController>().sideMoveState = true;
         block.GetComponent<BlockController>().fallState = true;
-        block.GetComponent<BlockController>().rotatinState = true;
         block.GetComponent<BlockController>().kidou = true;
         stockBlocks.RemoveAt(0);
         for (int i = 0; i < stockBlocks.Count; i++)
@@ -308,7 +291,7 @@ public class BlockManager : MonoBehaviour
         TextPrint();
     }
 
-    private void TextPrint()
+    public void TextPrint()
     {
         for (int i = 0; i < 21; i++)
         {
